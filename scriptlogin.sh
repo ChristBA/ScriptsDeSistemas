75bf10ab701bc63c39a38e4ef8e7de856d822e68
ChristBA <41087713+ChristBA@users.noreply.github.com>
2022-09-13 13:56:22 -0300

    Agregado script de login

--- scriptlogin.sh
+++ scriptlogin.sh
@@ -8,36 +8,44 @@ do
 	read opcion
 	case $opcion in
 		1)
-		echo "Ingrese su nombre: "
-		read nom
-		if [ -z $nom ]
+		echo "Ingrese su cédula"
+		read ced
+		if [ -z $ced ]
 		then
 			echo "El dato no puede estar vacio"
 		else
-			echo "Ingrese su contraseña"
-			read con
-			if [ -z $con ]
+			echo "Ingrese su nombre de usuario: "
+			read nom
+			if [ -z $nom ]
 			then
 				echo "El dato no puede estar vacio"
 			else
-				existe=$(grep -c "$nom" usuarios.txt)
-				if [ $existe -eq 0 ]
+				echo "Ingrese su contraseña"
+				read con
+				if [ -z $con ]
 				then
-					echo "$nom" >> usuarios.txt
-					echo "$con" >> contra.txt
+					echo "El dato no puede estar vacio"
 				else
-					echo "El usuario ya existe"
+					existe=$(grep -c "$nom" usu.txt)
+					if [ $existe -eq 0 ]
+					then
+						echo "$ced:$nom:$con" >> usu.txt
+					else
+						echo "El usuario ya existe"
+					fi
 				fi
 			fi
 		fi
 		;;
 		2)
+		echo "Ingrese su cedula"
+		read ced
 		echo "Ingrese su nombre de usuario"
 		read nom
 		echo "Ingrese su contraseña"
 		read con
-		existeusu=$(grep -c "$nom" usuarios.txt)
-		existepass=$(grep -c "$con" contra.txt)
+		existeusu=
+		existepass=
 		if [ $existeusu -eq 0 ]
 		then
 			echo "El usuario no existe"
